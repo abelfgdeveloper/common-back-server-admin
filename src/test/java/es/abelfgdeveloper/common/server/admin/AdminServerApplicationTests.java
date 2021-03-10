3e41e9f2a7017aca16708276dabfc9bb7ed4387e
Abel <abeldevelop@users.noreply.github.com>
2021-03-10 21:23:46 +0100

    Create the admin server

---
+++ src/test/java/es/abelfgdeveloper/common/server/admin/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.abelfgdeveloper.common.server.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class AdminServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
